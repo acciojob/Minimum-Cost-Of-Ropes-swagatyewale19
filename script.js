1752b2d4f72a433711c11b8e654b6b56ac2e4f28
Swagat Yewale <120251460+swagatyewale19@users.noreply.github.com>
2023-02-25 15:15:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function calculateMinCost() {
-  //your code here
+  // Calculate the cost of connecting the two ropes
+const combinedCost = rope1 + rope2;
+
+// Add the combined cost to the total cost
+cost += combinedCost;
+
+// Insert the combined rope into the heap
+minHeap.insert(combinedCost);
   
   
   
